984a6784ccc13cf8c1f23622e3c1b597642f88c6
godlovehie <godlovehie@eri.com.tw>
2021-10-26 11:52:24 +0800

    新資料

---
+++ src/pages/components/AddPageList.js
@@ -0,0 +1,201 @@
+import { useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { v4 } from "uuid";
+
+function AddPageList (props){
+  const {
+  setData,
+  data,
+  setSerialNumber,
+  serialNumber,
+  uuid,
+  pageName,
+  pageExplanation,
+  createPageUser,
+  createPageDate,
+  updatePageUser,
+  updatePageDate,
+  setPageName,
+  setPageExplanation,
+  setCreatePageUser,
+  setCreatePageDate,
+  setUpdatePageUser,
+  setUpdatePageDate,
+  inputStatus,
+  setInputStates,
+  editId
+} = props;
+
+  function pageNameChange(e) {
+    setPageName(e.target.value);
+  }
+  function pageExplanationChange(e) {
+    setPageExplanation(e.target.value);
+  }
+  function createPageUserChange(e) {
+    setCreatePageUser(e.target.value);
+  }
+
+  function createPageDateChange(e) {
+    setCreatePageDate(e.target.value);
+  }
+  function updatePageUserChange(e) {
+    setUpdatePageUser(e.target.value);
+  }
+
+  function updatePageDateChange(e) {
+    setUpdatePageDate(e.target.value);
+  }
+
+
+  //新增功能
+
+  function addItem() {
+    if (
+      pageName !== "" &&
+      pageExplanation !== "" &&
+      createPageUser !== "" &&
+      createPageDate !== ""
+    ) {
+      setData(function () {
+        return [
+          {
+            serialNumber,
+            uuid:v4(),
+            pageName,
+            pageExplanation,
+            createPageUser,
+            createPageDate,
+            updatePageUser,
+            updatePageDate,
+          },
+          ...data,
+        ];
+      });
+
+      setSerialNumber(serialNumber + 1);
+      setPageName("");
+      setPageExplanation("");
+      setCreatePageUser("");
+      setCreatePageDate("");
+  
+    } else {
+      alert("請填寫資料");
+    }
+  }
+
+  //修改功能
+  function upEditData(){
+    setInputStates(true)
+    setData((prev)=>{
+        const index = data.findIndex((item)=>{
+            return item.uuid!==uuid
+        })
+        const newData = [...prev]
+        newData[index]={
+        "serialNumber":editId,
+        "uuid":uuid,
+        "pageName": pageName,
+        "pageExplanation": pageExplanation,
+        "createPageUser": createPageUser,
+        "createPageDate": createPageDate,
+        "updatePageUser": updatePageUser,
+        "updatePageDate": updatePageDate,
+      }
+       return newData
+       
+    })
+    setPageName("")
+    setPageExplanation("")
+    setCreatePageUser("")
+    setCreatePageDate("")
+    setUpdatePageUser("")
+    setUpdatePageDate("")
+    alert("修改成功");
+}
+
+  //日期排序功能
+  useEffect(() => {
+    function changeSort(){
+      let newData = [...data]
+      newData = [...data].sort((a, b) => a.createPageDate >= b.createPageDate ? 1 :-1);
+      console.log(newData)
+      setData(newData)
+    }
+    changeSort();
+  },[...data]);
+
+
+  return (
+    <div>
+      <h5>MenuMaintain-新增/修改</h5>
+      <Table  striped bordered hover>
+        <tbody>
+          <tr>
+            <td className="height-td"
+            >頁面:</td>
+            <td className="width-td">
+              <input type="text" value={pageName} onChange={pageNameChange} />
+            </td>
+            <td className="height-td">頁面說明:</td>
+            <td className="cols-td">
+              <textarea
+                 cols="50" rows="3"
+                value={pageExplanation}
+                onChange={pageExplanationChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>建檔人員:</td>
+            <td>
+              <input
+                 disabled={!inputStatus && "disable"}
+                type="text"
+                value={createPageUser}
+                onChange={createPageUserChange}
+              />
+            </td>
+            <td>建檔日期:</td>
+            <td>
+              <input
+                disabled={!inputStatus && "disable"}
+                type="date"
+                value={createPageDate}
+                onChange={createPageDateChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>修改人員:</td>
+            <td>
+              <input
+                disabled={inputStatus && "disable"}
+                type="text"
+                value={updatePageUser}
+                onChange={updatePageUserChange}
+              />
+            </td>
+            <td>修改日期:</td>
+            <td>
+              <input
+                disabled={inputStatus && "disable"}
+                type="date"
+                value={updatePageDate}
+                onChange={updatePageDateChange}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <div className="addPage-button">
+      {inputStatus && <Button onClick={addItem}  variant="outline-secondary"className="add">新增</Button>}
+       {!inputStatus &&  <Button onClick={upEditData} variant="outline-secondary" className="update">修改</Button>}
+      </div>
+    </div>
+  );
+};
+
+export default AddPageList;
---
+++ src/pages/components/PagesList.js
@@ -0,0 +1,85 @@
+import React from "react";
+import DeletePageItem from "./DeletePageItem";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const PagesList = ({ 
+  data,
+  uuid,
+  setData,
+  displayData,
+  setPageName,
+  setSerialNumber,
+  setPageExplanation,
+  setCreatePageUser,
+  setCreatePageDate,
+  setUpdatePageUser,
+  setUpdatePageDate,
+  setInputStates,
+  setEditId,
+  setUuid,
+
+ }) => {
+  
+  
+  return (
+    <div className="PagesList">
+      <h5>MenuMaintain-表單</h5>
+      <Table striped bordered hover>
+            <thead>
+              <tr>
+                 <th>序號</th>
+                 <th>頁面編碼</th>
+                 <th>頁面</th>
+                 <th>頁面內容</th>
+                 <th>建檔人員</th>
+                 <th>建檔日期</th>
+                 <th>修改人員</th>                                                                                                                                                                                   
+                 <th>修改日期</th>
+                 <th>修改/刪除</th>
+              </tr>
+              </thead>
+                     
+              {displayData.map((item , index) => {
+              const { pageName, pageExplanation, createPageUser, 
+              createPageDate, updatePageUser, updatePageDate,uuid,
+               serialNumber,changeSort,setDisplayData,displayData} = item;
+               
+
+              return (
+                <DeletePageItem
+                  {...item}
+                  item={item}
+                  index={index} 
+                  serialNumber={serialNumber}
+                  uuid={uuid}
+                  pageName={pageName}
+                  pageExplanation={pageExplanation}
+                  createPageUser={createPageUser}
+                  createPageDate={createPageDate}
+                  updatePageUser={updatePageUser}
+                  updatePageDate={updatePageDate}
+                  setPageName={setPageName}
+                  setId={setUuid}
+                  setSerialNumber={setSerialNumber}
+                  setPageExplanation={setPageExplanation}
+                  setCreatePageUser={setCreatePageUser}
+                  setCreatePageDate={setCreatePageDate}
+                  setUpdatePageUser={setUpdatePageUser}
+                  setUpdatePageDate={setUpdatePageDate}
+                  setData={setData}
+                  data={data}
+                  changeSort={changeSort}
+                  setDisplayData={setDisplayData}
+                  displayData={displayData}
+                  setInputStates={setInputStates}
+                  setEditId={setEditId}
+                />
+              );
+              })
+ }
+        </Table>
+    </div>
+  );
+};
+export default PagesList;
