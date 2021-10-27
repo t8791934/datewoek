02459acc67833c5d95de703a8f02a6e36215664f
godlovehie <godlovehie@eri.com.tw>
2021-10-27 09:52:42 +0800

    更新

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React  from "react";
-import { render,scree } from "@testing-library/react";
-import App from "./App.js";
-
-test('render learn react link', () => {
-    render(<App />);
-    const linkElement = scree.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-})
--- src/pages/components/Header.js
+++
@@ -1,7 +0,0 @@
-
-const Header = () => {
-    return(
-    <h1 className="header-h1">MenuMaintain </h1>
-    )
-}
-export default Header;
